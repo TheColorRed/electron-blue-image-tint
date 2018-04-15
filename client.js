33d4b1a22f589fb1ef34671662943e415651c3fb
Ryan <untuned20@gmail.com>
2018-04-15 11:20:37 -0500

    Init

---
+++ client.js
@@ -0,0 +1,12 @@
+const { ipcRenderer } = require('electron')
+
+ipcRenderer.addListener('paste-image', (e, img, size) => {
+  let canvas = document.createElement('canvas')
+  let ctx = canvas.getContext('2d')
+  canvas.width = size.width
+  canvas.height = size.height
+  document.body.appendChild(canvas)
+
+  let data = new ImageData(new Uint8ClampedArray(img), size.width, size.height)
+  ctx.putImageData(data, 0, 0)
+})
---
+++ index.js
@@ -0,0 +1,56 @@
+const { app, BrowserWindow, ipcMain, WebContents, clipboard, ipcRenderer, Menu, nativeImage } = require('electron')
+const path = require('path')
+
+let mainWindow = null
+
+const createWindow = () => {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    show: false
+  })
+
+  let views = path.join(__dirname, '../../views')
+  global.web = mainWindow.webContents
+  mainWindow.loadURL(`file://${path.join(__dirname, 'index.html')}`)
+
+  mainWindow.on('closed', () => {
+    mainWindow = null
+  })
+
+  global.web.on('dom-ready', e => {
+    mainWindow.webContents.openDevTools()
+    mainWindow && mainWindow.show()
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
+
+let template = [
+  {
+    label: 'Edit',
+    submenu: [{ label: 'Paste', click: () => pasteImage() }]
+  }
+]
+
+let menu = Menu.buildFromTemplate(template)
+Menu.setApplicationMenu(menu)
+
+function pasteImage() {
+  let img = clipboard.readImage()
+  console.log('send paste message')
+  mainWindow && mainWindow.webContents.send('paste-image', img.toBitmap(), img.getSize())
+}
